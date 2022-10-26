19a71c0bac8b90f289396ec7eeabd39befa42dc2
Jsaano <95000166+Jsaano@users.noreply.github.com>
2022-10-25 23:23:33 -0300

    third commit
    correccion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import "./styles.css";
-import { useState , useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
-import Home from "./views/Home";
-import Galeria from './components/Galeria';
-import Favoritos from "./views/Favoritos";
-import UserContext from "./UserContext";
+import Navbar  from "./components/Navbar";
+import Home from "./components/Home";
+import Pokemones from "./Pokemones";
 
 export default function App() {
 
   return (
-    <div className="App">
-      <UserContext.Provider value="null">
-        <BrowserRouter>
-          <Navbar />
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favoritos" element={<Favoritos />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/pokemon/:id" element={< Pokemones />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
